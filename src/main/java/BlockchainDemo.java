09f8efb4f692bda55bf5aa490a9b868536c5b692
Gustavo Santos <gustavosci@live.com>
2018-09-25 08:22:33 -0300

    inserida validação da blockchain e lógica para mineração dos blocos

--- src/main/java/BlockchainDemo.java
+++ src/main/java/BlockchainDemo.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.List;
 
 import com.google.gson.GsonBuilder;
 
@@ -6,15 +7,56 @@ import domain.Block;
 
 public class BlockchainDemo {
 
-    public static ArrayList<Block> blockchain = new ArrayList<Block>();
+    private static final Integer DIFFICULTY_MINE_BLOCK = 5;
+
+    public static List<Block> blockchain = new ArrayList<Block>();
 
     public static void main(String[] args) {
 
         blockchain.add(new Block("Hi im the first block", "0"));
-        blockchain.add(new Block("Yo im the second block", blockchain.get(blockchain.size() - 1).getHash()));
-        blockchain.add(new Block("Hey im the third block", blockchain.get(blockchain.size() - 1).getHash()));
+        System.out.println("Trying to Mine block 1... ");
+        blockchain.get(0).mineBlock(DIFFICULTY_MINE_BLOCK);
 
+        blockchain.add(new Block("Yo im the second block", blockchain.get(blockchain.size() - 1).getHash()));
+        System.out.println("Trying to Mine block 2... ");
+        blockchain.get(1).mineBlock(DIFFICULTY_MINE_BLOCK);
+
+        blockchain.add(new Block("Hey im the third block", blockchain.get(blockchain.size() - 1).getHash()));
+        System.out.println("Trying to Mine block 3... ");
+        blockchain.get(2).mineBlock(DIFFICULTY_MINE_BLOCK);
+
+        System.out.println("\nEsta blockchain é válida? " + isChainValid());
+
+        System.out.println("Prazer, eu sou a blockchain:\n");
         String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
         System.out.println(blockchainJson);
+
+        System.out.println("\nAdulterando a blockchain...");
+        blockchain.get(1).setData("New data");
+        System.out.println("\nEsta blockchain é válida? " + isChainValid());
+    }
+
+    public static Boolean isChainValid() {
+
+        Block currentBlock;
+        Block previousBlock;
+
+        for (int i = 1; i < blockchain.size(); i++) {
+
+            currentBlock = blockchain.get(i);
+            previousBlock = blockchain.get(i - 1);
+
+            if (!currentBlock.getHash().equals(currentBlock.calculateHash())) {
+                System.out.println("Hash do bloco atual (" + i + ") é inválido!");
+                return false;
+            }
+
+            if (!previousBlock.getHash().equals(previousBlock.calculateHash())) {
+                System.out.println("Hash do bloco anterior (" + (i - 1) + ") é inválido!");
+                return false;
+            }
+        }
+
+        return true;
     }
 }
--- src/main/java/domain/Block.java
+++ src/main/java/domain/Block.java
@@ -14,15 +14,32 @@ public class Block {
 
     private Long timestamp;
 
+    private Integer nonce; // times for hash generate
+
     public Block(final String data, final String previousHash) {
         this.previousHash = previousHash;
         this.data = data;
         this.timestamp = new Date().getTime();
+        this.nonce = 0;
         this.hash = calculateHash();
     }
 
-    private String calculateHash() {
-        return StringUtil.applySha256(previousHash + Long.toString(timestamp) + data);
+    public String calculateHash() {
+        return StringUtil.applySha256(previousHash + Long.toString(timestamp) + data + Integer.toString(nonce));
+    }
+
+    public void mineBlock(final Integer difficulty) { // número de "0" necessários para resolver o hash do bloco
+
+        // A dificuldade de mineração desta blockchain é encontrar o primeiro hash com X "0" no início, onde X é difficulty
+
+        final String target = new String(new char[difficulty]).replace('\0', '0');
+
+        while(!hash.substring( 0, difficulty).equals(target)) {
+            nonce ++;
+            hash = calculateHash();
+        }
+
+        System.out.println("Bloco minerado: " + hash);
     }
 
     public String getHash() {
